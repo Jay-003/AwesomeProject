734a34d28d9ef6ea9b80cc212668d5306576ec37
Mihir Patel <mihir.patel@dal.ca>
2023-04-30 22:47:36 -0300

    Check push

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, Image } from "react-native"
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hello!</Text>
       <Image
         source={require("./image/logo.jpeg")}
         style={{ width: 250, height: 100 }}
